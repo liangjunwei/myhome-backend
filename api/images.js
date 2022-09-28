e049a3490f6122e5d85ba912e6d01853c013f64f
liangjunwei <flashwei123@gmail.com>
2022-09-28 01:08:57 -0700

    implemented aws s3 for image uploading

--- api/images.js
+++ api/images.js
@@ -1,6 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 const router = express.Router();
+
 import multer from 'multer';
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import crypto from 'crypto';
+
+const bucketName = process.env.BUCKET_NAME;
+const bucketRegion = process.env.BUCKET_REGION;
+const accessKey = process.env.ACCESS_KEY;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+const s3 = new S3Client({
+    credentials: {
+        accessKeyId: accessKey,
+        secretAccessKey: secretAccessKey
+    },
+    region: bucketRegion
+});
+
+const randomImageName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex');
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -10,6 +30,19 @@ router.post('/upload', upload.array('images', 10), async (req, res, next) => {
     console.log('req.body:', req.body);
     console.log('req.files:', req.files);
     try {
+        for(let i = 0; i < req.files.length; i++) {
+
+            const params = {
+                Bucket: bucketName,
+                Key: randomImageName(),
+                Body: req.files[i].buffer,
+                ContentType: req.files[i].mimetype
+            }
+            
+            const command = new PutObjectCommand(params);
+            await s3.send(command);
+        }
+
         res.send({
             message: "Images Uploaded!"
         });
